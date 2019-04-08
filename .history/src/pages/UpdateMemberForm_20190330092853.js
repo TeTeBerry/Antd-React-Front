8424c611b4926cd9d39386393858c324042b13d5
berry <berry@berrydeMacBook-Pro.local>
2019-04-09 02:07:15 +0700

    create client file

---
+++ .history/src/pages/UpdateMemberForm_20190330092853.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import { Modal, Form, Input,InputNumber} from 'antd';
+
+
+const CollectionUpdateForm = Form.create({ name: 'form_in_modal' })(
+    class UpdateMember extends Component {
+        state = {
+            disabled: true,
+          };
+  
+     
+      render() {
+        const {
+          visible, onCancel, handleSubmit, form,
+        } = this.props;
+        const { getFieldDecorator } = form;
+  
+        return (
+          <Modal
+            visible={visible}
+            title='Edit Form'
+            okText="OK"
+            onCancel={onCancel}
+            onOk={handleSubmit}
+          
+          >
+           <Form layout="vertical">
+              <Form.Item label="ID">
+              {getFieldDecorator('_id', {
+                  rules: [{ required: true, message: 'Please input member name!' }],
+                })(
+                    <InputNumber  disabled={this.state.disabled} />
+                )}
+              </Form.Item>
+             
+              <Form.Item label="Member Name">
+              {getFieldDecorator('membername', {
+                rules: [{ required: true, message: 'Please input member name!' }],
+              })(
+                <Input />
+              )}
+            </Form.Item>
+              <Form.Item label="Phone Number">
+              {getFieldDecorator('tel', {
+                  rules: [{ required: true, message: 'Please input phone number!' }],
+                })(
+                  <Input />
+                )}
+              
+              </Form.Item>
+              <Form.Item label="Room Number">
+              {getFieldDecorator('room', {
+                  rules: [{ required: true, message: 'Please input room number!' }],
+                })(
+                  <Input />
+                )}
+              
+              </Form.Item>
+              <Form.Item
+            label="E-mail"
+          >
+            {getFieldDecorator('email', {
+              rules: [{
+                type: 'email', message: 'The input is not valid E-mail!',
+              }, {
+                required: true, message: 'Please input your E-mail!',
+              }],
+            })(
+              <Input />
+            )}
+          </Form.Item>
+            </Form>
+          </Modal>
+        );
+      }
+    }
+  );
+
+  export default CollectionUpdateForm;
