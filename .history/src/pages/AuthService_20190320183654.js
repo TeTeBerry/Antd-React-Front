6fa4f24e44bd932d6ff6a6ba90577a3e20bf457e
TeTeBerry <250714751@qq.com>
2019-06-14 05:47:01 +0700

    rm history

--- .history/src/pages/AuthService_20190320183654.js
+++
@@ -1,8 +0,0 @@
-import decode from 'jwt-decode';
-export default class AuthService {
-    //Initializing important variables
-    constructor(domain) {
-        this.domain = domain || 'http://localhost:4000'//API service domain
-
-    }
-}
