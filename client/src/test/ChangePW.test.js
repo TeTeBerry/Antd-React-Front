d611a31a1e0e206d103d8472849f272025639647
TeTeBerry <250714751@qq.com>
2019-06-21 08:23:29 +0700

    test changepw

--- client/src/test/ChangePW.test.js
+++ client/src/test/ChangePW.test.js
@@ -2,8 +2,6 @@ import React from "react";
 import setTest from "../setupTests";
 import { shallow, render, mount } from "enzyme";
 import ChangePW from "../pages/adminpage/ChangePW";
-import { Select, Form, Icon, Input, Button, message } from "antd";
-import { fn } from "moment";
 
 describe("Test case for meter", () => {
   it("Input render correctly", () => {
@@ -19,6 +17,5 @@ describe("Test case for meter", () => {
 
     const wrapper = mount(<ChangePW {...props} />);
     expect(wrapper.props().oldPwd).toEqual("123456");
-    expect(wrapper.props().onChange).toBe(true);
   });
 });
--- client/src/test/Member.test.js
+++ client/src/test/Member.test.js
@@ -1,11 +1,32 @@
 import React from "react";
 import setTest from "../setupTests";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import Member from "../pages/memberpage/Member";
 
 describe("Test case for member", () => {
-  it("get member list ", () => {
+  it("get member list dataSource of table", () => {
     const wrapper = shallow(<Member user={{ memberList: [] }} />);
     expect(wrapper.find("Table").props().dataSource).toEqual([]);
+  });
+  it("get member list", () => {
+    const props = {
+      mid: 1,
+      meterName: "sensor1",
+      meterDesc: "made in 2019",
+      memberName: "tete00111",
+      room: "201",
+      memberContact: "12323131",
+      createDate: "2019-06-18T07:01:21.000+0000"
+    };
+    const wrapper = mount(<Member {...props} />);
+    expect(wrapper.props()).toEqual({
+      mid: 1,
+      meterName: "sensor1",
+      meterDesc: "made in 2019",
+      memberName: "tete00111",
+      room: "201",
+      memberContact: "12323131",
+      createDate: "2019-06-18T07:01:21.000+0000"
+    });
   });
 });
