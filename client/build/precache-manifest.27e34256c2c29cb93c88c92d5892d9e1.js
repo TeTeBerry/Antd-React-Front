81e823442a66abaa90c6cb8a284c5d7bebb26d7b
TeTeBerry <250714751@qq.com>
2019-07-13 02:26:08 +0700

    build

---
+++ client/build/precache-manifest.27e34256c2c29cb93c88c92d5892d9e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4608943c67540b978844",
+    "url": "/static/js/main.bdb58241.chunk.js"
+  },
+  {
+    "revision": "01078c4138214a5487ae",
+    "url": "/static/js/2.2524362a.chunk.js"
+  },
+  {
+    "revision": "4608943c67540b978844",
+    "url": "/static/css/main.c9c712ad.chunk.css"
+  },
+  {
+    "revision": "01078c4138214a5487ae",
+    "url": "/static/css/2.a79632c6.chunk.css"
+  },
+  {
+    "revision": "8efaedcaa0b6468140d2e2641855b6cb",
+    "url": "/index.html"
+  }
+];
